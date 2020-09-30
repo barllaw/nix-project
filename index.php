d42204f0507227323093b4e8b4b28ccc1ab87c38
barllaw <londofff@ukr.net>
2020-09-30 15:02:38 +0300

    not have changes

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
 session_start();
 
@@ -30,6 +29,3 @@ function head($title){
 function footer(){
     include 'html/footer.php';
 }
-=======
-echo 'Hello, world!';
->>>>>>> bee6a75246a75440b0e8386cc8ef428f1af549fd
